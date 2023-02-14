0ae552ef07c4ed7193cbe23018439ff5978e1ce4
Anwalt98 <ilya.ns1998@gmail.com>
2023-02-15 00:08:02 +0300

    добавил переменную BROWSER

--- src/main/java/framework/Constants.java
+++ src/main/java/framework/Constants.java
@@ -1,9 +1,15 @@
 package framework;
 
 public class Constants {
+
     public static final String LETTERS_UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
     public static final String NUMBERS = "0123456789";
+
     public static final String LETTERS_LOWER_CASE = "abcdefghijklmnopqrstuvxyz";
+
     public static final String CYRRILIC_LOWER_CASE = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+
     public static final int RANDOM_STRING_SIZE = 10;
+
 }
